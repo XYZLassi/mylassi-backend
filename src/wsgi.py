6d912447aa5b73310bbefe46738158bdfe13cb44
Lassi <csharplassi@posteo.de>
2023-01-12 19:16:53 +0100

    Update: wsgi.py

--- src/wsgi.py
+++ src/wsgi.py
@@ -1,3 +1,15 @@
+from starlette.middleware.cors import CORSMiddleware
+
 from mylassi_backend import create_app
 
+origins = ["*"]
+
 app = create_app()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
