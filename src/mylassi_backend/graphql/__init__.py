b3ab876fc79b54549b3d48ecc7fbe7280bee8557
Lassi <csharplassi@posteo.de>
2023-01-29 19:07:49 +0100

    Add scoped_session for graphQL

--- src/mylassi_backend/graphql/__init__.py
+++ src/mylassi_backend/graphql/__init__.py
@@ -3,25 +3,30 @@ __all__ = ['graphql_app']
 from typing import List
 
 import strawberry
+from sqlalchemy.orm import scoped_session, Session
 from strawberry.fastapi import GraphQLRouter
 
+from mylassi_data.db import SessionLocal
 from mylassi_data.models import *
 from mylassi_data.graphschema import *
+
+# noinspection PyTypeChecker
+db_session: Session = scoped_session(SessionLocal)
 
 
 @strawberry.type
 class Query:
     @strawberry.field
     def articles(self) -> List[ArticleGraphType]:
-        return ArticleModel.q().all()
+        return ArticleModel.q(db_session).all()
 
     @strawberry.field
     def authors(self) -> List[AuthorGraphType]:
-        return UserModel.q().filter(UserModel.article_count > 0).all()
+        return UserModel.q(db_session).filter(UserModel.article_count > 0).all()
 
     @strawberry.field()
     def categories(self) -> List[CategoryGraphType]:
-        return CategoryModel.q().all()
+        return CategoryModel.q(db_session).all()
 
 
 graphql_schema = strawberry.Schema(Query)
