e52a9f33fc036c6b1af75aad8bd32dea45511c0e
Lassi <csharplassi@posteo.de>
2023-02-24 18:44:05 +0100

    Add: Pagination for articles in qraphQL

--- src/mylassi_backend/graphql/__init__.py
+++ src/mylassi_backend/graphql/__init__.py
@@ -1,23 +1,42 @@
 __all__ = ['graphql_app']
 
-from typing import List, Union, Optional
+from typing import List, Optional
 
 import strawberry
 from sqlalchemy.orm import scoped_session, Session
 from strawberry.fastapi import GraphQLRouter
 
 from mylassi_data.db import SessionLocal
-from mylassi_data.models import *
 from mylassi_data.graphschema import *
+from mylassi_data.models import *
+from mylassi_tools.pagination import encode_cursor, decode_cursor
 
 
 # noinspection PyTypeChecker
 @strawberry.type
 class Query:
     @strawberry.field
-    def articles(self) -> List[ArticleGraphType]:
+    def articles(self, cursor: Optional[str] = None, size: int = 5) -> PaginationResult[ArticleGraphType]:
         session: Session = scoped_session(SessionLocal)
-        return ArticleModel.q(session).filter(ArticleModel.is_deleted_flag == None).all()
+
+        if size <= 0 or size > 50:
+            size = 5
+
+        query = ArticleModel.q(session)
+        query = query.filter(ArticleModel.is_deleted_flag == None)
+        query = query.order_by(ArticleModel.id.desc())
+
+        if cursor and (cursor_id := decode_cursor(cursor)):
+            query = query.filter(ArticleModel.id < cursor_id)
+
+        query = query.limit(size)
+        items = query.all()
+
+        return PaginationResult(
+            items=items,
+            cursor=encode_cursor(items[-1].id) if len(items) > 0 else None,
+            size=size,
+        )
 
     @strawberry.field
     def article_by_id(self, article: int) -> Optional[ArticleGraphType]:
