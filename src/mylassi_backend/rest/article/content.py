705556a4fb3f0cb95f2e070fdfb2940ed2056f66
Lassi <csharplassi@posteo.de>
2023-03-06 18:43:44 +0100

    Add: update_article_content

--- src/mylassi_backend/rest/article/content.py
+++ src/mylassi_backend/rest/article/content.py
@@ -32,6 +32,28 @@ def get_article_content(article: int, content: int, session: Session = Depends(g
     return content.rest_type()
 
 
+@router.post("/{article}/content/{content}", response_model=ArticleContentRestType,
+             operation_id='updateArticleContent')
+def update_article_content(article: int,
+                           content: int,
+                           options: ArticleContentOptionsRestType = Body(embed=False),
+                           current_user: UserModel = Depends(get_current_active_user),
+                           session: Session = Depends(get_db)) \
+        -> ArticleContentRestType:
+    article: ArticleModel = ArticleModel.get_or_404(session, article)
+    content: ArticleContentModel = ArticleContentModel.get_or_404(session, content)
+
+    assert content in article.contents
+
+    if article.author != current_user and not current_user.is_admin:
+        raise HTTPException(status_code=404, detail="Item not found")
+
+    content.set_from_rest_type(options)
+    session.commit()
+
+    return content.rest_type()
+
+
 @router.post("/{article}/content/", response_model=List[ArticleContentRestType],
              operation_id='addArticleContent', name='Add Contents To article')
 @router.put("/{article}/content/", response_model=List[ArticleContentRestType],
